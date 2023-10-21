162ba8f73f1e8cadbed0ef3ade5d044b9971b864
Charilaos Alkiviades Savoullis <casavoullis@gmail.com>
2023-10-21 21:47:00 +0100

    added example scripts to run sql queries

---
+++ example/queries_skills.py
@@ -0,0 +1,51 @@
+import sys
+import os
+
+# Get the current script's directory
+current_script_directory = os.path.dirname(os.path.realpath(__file__))
+
+# Add the "src" directory to the Python path
+src_directory = os.path.join(current_script_directory, "..", "src")
+sys.path.append(src_directory)
+
+from database_connector import create_connection, close_connection
+from db_functions import *
+
+
+""" 
+Run this script from the src directory 
+python ../example/queries_person_food.py 
+"""
+
+
+
+# Create a database connection
+db_connection = create_connection()
+
+# Create a cursor
+mycursor = db_connection.cursor()
+
+# Select the database
+mycursor.execute("USE Database_python")
+
+
+# Modify the query according to your needs
+query = """
+    SELECT *
+    FROM Skills
+"""
+
+result = execute_query(mycursor, query)
+
+# Process the result as needed
+if result is not None:
+    for row in result:
+        print(row)
+else:
+    print("No results found.")
+
+
+
+# Close the cursor and connection
+mycursor.close()
+close_connection(db_connection)
