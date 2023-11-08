4484e1e89763b8bf71fca0b6734de23257332529
Charilaos Alkiviades Savoullis <casavoullis@gmail.com>
2023-11-08 05:30:00 +0000

    added logging messages

--- src/database_connector.py
+++ src/database_connector.py
@@ -1,27 +1,39 @@
 import mysql.connector
 from configparser import ConfigParser
+import logging
 
+# Function for database connection with error handling
 def create_connection():
-    def read_db_config(filename='database_config.ini', section='MySQL'):
-        parser = ConfigParser()
-        parser.read(filename)
-        db_config = {}
-        if parser.has_section(section):
-            params = parser.items(section)
-            for param in params:
-                db_config[param[0]] = param[1]
-        else:
-            raise Exception(f"Section {section} not found in the {filename} file")
-        return db_config
+    try:
+        def read_db_config(filename='database_config.ini', section='MySQL'):
+            parser = ConfigParser()
+            parser.read(filename)
+            db_config = {}
+            if parser.has_section(section):
+                params = parser.items(section)
+                for param in params:
+                    db_config[param[0]] = param[1]
+            else:
+                raise Exception(f"Section {section} not found in the {filename} file")
+            return db_config
 
-    db_config = read_db_config()
-    connection = mysql.connector.connect(
-        host=db_config['host'],
-        user=db_config['user'],
-        password=db_config['password'],
-        port=db_config['port']
-    )
-    return connection
+        db_config = read_db_config()
+        connection = mysql.connector.connect(
+            host=db_config['host'],
+            user=db_config['user'],
+            password=db_config['password'],
+            port=db_config['port']
+        )
+        logging.info("Database connection established successfully.")
+        return connection
+    except Exception as e:
+        logging.error(f"Error establishing database connection: {e}")
+        raise
 
 def close_connection(connection):
-    connection.close()
+    try:
+        connection.close()
+        logging.info("Database connection closed successfully.")
+    except Exception as e:
+        logging.error(f"Error closing database connection: {e}")
+        raise
--- src/db_functions.py
+++ src/db_functions.py
@@ -1,21 +1,60 @@
+import logging
 
 def drop_and_create_table(mycursor, table_name, table_definition):
     mycursor.execute(f"SHOW TABLES LIKE '{table_name}'")
     table_exists = mycursor.fetchone()
 
     if table_exists:
-        mycursor.execute(f"DROP TABLE {table_name}")
-        print(f"The table {table_name} has been dropped.")
-    
-    mycursor.execute(table_definition)
+        try:
+            mycursor.execute(f"DROP TABLE {table_name}")
+            logging.info(f"The table {table_name} has been dropped.")
+        except Exception as drop_error:
+            logging.error(f"Error dropping table {table_name}: {drop_error}")
+            raise
+
+    try:
+        mycursor.execute(table_definition)
+        logging.info(f"The table {table_name} has been created.")
+    except Exception as create_error:
+        logging.error(f"Error creating table {table_name}: {create_error}")
+        raise
+
 
 def print_table_description(mycursor, table_name):
-    mycursor.execute(f"DESCRIBE {table_name}")
-    print(f"{table_name} Table Description:")
-    for row in mycursor.fetchall():
-        print(row)
-    print("")
+    try:
+        mycursor.execute(f"DESCRIBE {table_name}")
+        logging.debug(f"{table_name} Table Description:")
+        for row in mycursor.fetchall():
+            logging.debug(row)
+        logging.debug("")
+        logging.info(f"Table description for {table_name} printed successfully.")
+    except Exception as e:
+        logging.error(f"Error printing table description: {e}")
+        raise
+
 
 def insert_data(mycursor, records, insert_query):
-    for record_data in records:
-        mycursor.execute(insert_query, record_data)
+    try:
+        for record_data in records:
+            mycursor.execute(insert_query, record_data)
+        logging.info(f"Data inserted successfully using query: {insert_query}")
+    except Exception as e:
+        logging.error(f"Error inserting data: {e}")
+        raise
+
+
+def execute_query(mycursor, query, params=None):
+    try:
+        if params:
+            mycursor.execute(query, params)
+        else:
+            mycursor.execute(query)
+        
+        # Fetch all results before returning
+        result = mycursor.fetchall()
+
+        logging.info(f"Query executed successfully: {query}")
+        return result
+    except Exception as e:
+        logging.error(f"Error executing query: {e}")
+        raise
